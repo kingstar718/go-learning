72c1975ba97bee60af4cc84a31cefffedff51487
kingstar718 <kingstar718@foxmail.com>
2021-03-07 23:14:50 +0800

    array

---
+++ leetcode/array/0088MergeSortedArray.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+/**
+合并两个已经有序的数组，结果放在第一个数组中，第一个数组假设空间足够大。要求算法时间复杂度足够低。
+*/
+
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	if m == 0 {
+		copy(nums1, nums2)
+		return
+	}
+
+	i := m - 1
+	j := n - 1
+	k := m + n - 1
+
+	//从后往前放，循环一次即可
+	for ; i >= 0 && j >= 0; k-- {
+		if nums1[i] > nums2[j] {
+			nums1[k] = nums1[i]
+			i--
+		} else {
+			nums1[k] = nums2[j]
+			j--
+		}
+	}
+	for ; j >= 0; k-- {
+		nums1[k] = nums2[j]
+		j--
+	}
+}
+
+func main() {
+	nums1 := []int{1, 2, 3, 0, 0, 0}
+	nums2 := []int{2, 5, 6}
+	merge(nums1, 3, nums2, 3)
+	fmt.Println(nums1)
+}
---
+++ leetcode/array/0118Pascal'sTriangle.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+/**
+给定一个非负整数，生成杨辉三角的前numRows行
+每个数是它左上方和右上方之和
+*/
+
+func generate(numRows int) [][]int {
+	var result [][]int
+	for i := 0; i < numRows; i++ {
+		var row []int
+		for j := 0; j < i+1; j++ {
+			if j == 0 || j == i {
+				row = append(row, 1)
+			} else if i > 1 {
+				row = append(row, result[i-1][j-1]+result[i-1][j])
+			}
+		}
+		result = append(result, row)
+	}
+	return result
+}
+
+func main() {
+	fmt.Println(generate(5))
+}
